0b39ad0922d12b92e0f755db83020ae80f4c81e2
BHakti Jadhav <bhaktijadhav12062006@gmail.com>
2025-07-06 22:23:05 +0530

    Arithmetic Opreatiors

--- Day 3/app.js
+++ Day 3/app.js
@@ -1,13 +1,16 @@
 let a = 10 ;
 let b = 20 ;
+
  let sum = 0 ;
  let sub = 0 ;
  let mul = 0 ;
  let div = 0 ;
+
  sum = a + b ;
  sub = a - b ;
  mul = a * b ;
  div = a / b ;
+ 
  console.log("sum of variables is " ,sum);
 console.log("substraction of variables is " ,sub);
 console.log("mul of variables is " ,mul);
