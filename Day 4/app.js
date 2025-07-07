9b58373cd7c76c0c98762b78261029dc8cbe5b19
BHakti Jadhav <bhaktijadhav12062006@gmail.com>
2025-07-07 22:45:26 +0530

    Loops

--- Day 4/app.js
+++ Day 4/app.js
@@ -2,3 +2,12 @@
 for (let a = 1 ; a<=100 ; a++) {
 console.log(a);
 }
+
+console.log ("_________________________________") ;
+
+// while loop
+let a = 1 ;
+a++ ;
+while (a<=100) {
+    console.log(a) ;
+}
